48a77e8a8a6f94cfc11b47bf77197bbb4ffc4e3b
Trevor Lee <trevorwslee@gmail.com>
2021-12-10 21:29:41 +0800

    commit

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
-import setuptools
+from distutils.core import setup
+from setuptools import find_packages
+import os
 
-with open("README.md", "r", encoding="utf-8") as fh:
-  long_description = fh.read()
+current_directory = os.path.dirname(os.path.abspath(__file__))
+try:
+    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
 
-setuptools.setup(
+setup(
   name='DDWifiBridge',
-  version='0.0.1',
+  version='0.7.8',
   author='Trevor Lee',
   author_email='trev_lee@hotmail.com',
   description='DumbDisplay Wifi Bridge for DumbDisplay Arduino Lirary',
   long_description=long_description,
   long_description_content_type="text/markdown",
   url='https://github.com/trevorwslee/DDWifiBridge',
-  project_urls = {
-  },
   license='MIT',
-  packages=[],
-  install_requires=[],
+  packages=find_packages('.')
 )
